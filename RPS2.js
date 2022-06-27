33bb8c946c54f847aaecc7d5dd2566dc62823d3e
Mike Reed <reed1mw@yahoo.com>
2022-06-27 14:53:57 -0500

    Add working game using alerts

--- RPS2.js
+++ RPS2.js
@@ -20,15 +20,51 @@ if (computerRandomNumber == 0) {
 } else if (computerRandomNumber == 1) {
     computerPlay = "paper";
 } else {
-    computerPlay = "scissors"
+    computerPlay = "scissors";
 }
 
 
  humanPrompt();
  computerMathRandom();
 
+ function playRound (humanPlay, computerPlay) {
+
+    if (humanPlay === "rock") {
+         if (computerPlay === "rock") {  
+            alert("It's a tie, rock vs rock!")
+        } else if (computerPlay === "scissors") {
+            alert ("You win, rock beats scissors!")
+        } else {
+            alert ("you lose, paper beats rock!")
+        }
+    }
+
+    else if (humanPlay === "paper") {
+        if (computerPlay === "paper") {
+            alert("It's a tie, paper vs paper!")
+        } else if (computerPlay === "rock"){
+            alert("You win, paper beats rock!")
+        } else {
+            alert ("You lose, scissors beats paper!")
+        }
+    }
+
+    else {
+        if (computerPlay === "scissors") {
+            alert("It's a tie, scissors vs scissors!")
+        } else if (computerPlay === "paper") {
+            alert("You win, scissors beats paper!")
+        } else {
+            alert("You lose, rock beats scissors!")
+        }
+    }
+ }
+
+ playRound(humanPlay, computerPlay);
+
 console.log(humanPlay);
 console.log(computerRandomNumber);
 console.log(computerPlay);
+
 
 
